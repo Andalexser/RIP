a90f307ca3c17bfd6cf1c923522200e89c3bee8e
Andalexser <71727504+Andalexser@users.noreply.github.com>
2021-11-17 18:50:04 +0300

    Delete Lab№3/lab_python_fp directory

--- Lab№3/lab_python_fp/gen_random.py
+++
@@ -1,9 +0,0 @@
-import random as rand
-
-def gen_random(num_count, begin, end):
-    for i in range(num_count):
-        yield rand.randint(begin, end)
-
-if __name__ == "__main__":
-    gen_random(10, 0, 100)
-    wait = input("PRESS ENTER TO CONTINUE.")
